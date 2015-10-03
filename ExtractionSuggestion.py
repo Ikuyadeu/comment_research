5ded2ff457564b0830159ac10e19294fb27fb426
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-10-03 21:35:02 +0900

    create tf-idf process

--- ExtractionSuggestion.py
+++ ExtractionSuggestion.py
@@ -12,23 +12,6 @@ import math
 import MySQLdb
 import treetaggerwrapper
 
-# Term Frequency
-def tf(word, document):
-    return document.words.count(word) / len(document.words)
-
-def n_containing(word, documents):
-    return sum(1 for document in documents if word in document)
-
-# Inverse Document Frequency
-def idf(word, documents):
-    return math.log(len(documents) / (1 + n_containing(word, documents)))
-
-def tfidf(word, document, documents):
-    return tf(word, document) * idf(word, documents)
-
-
-
-
 voteComments = [[],[]]
 voteComments[0].append(r'Patch Set [1-9]*: Looks good to me, but someone else must approve')
 voteComments[0].append(r'Patch Set [1-9]*: Works for me')
@@ -48,12 +31,12 @@ voteComments[1].append(r'Patch Set [1-9]*: Sanity problems found')
 # If Score isn't '+1' or '-1' -> return 0
 ######
 def JudgeVoteScore(m): # (Regular expression)
-    # Score +1
+	# Score +1
 	for comment in voteComments[0]:
 		if re.compile(comment).match(m):
 			return 1
 
-    # Score -1
+	# Score -1
 	for comment in voteComments[1]:
 		if re.compile(comment).match(m):
 			return -1
@@ -67,6 +50,14 @@ cnct = MySQLdb.connect(db="qt",user="root", passwd="password")
 csr = cnct.cursor()
 time_csr = cnct.cursor()
 
+fv_tf = []					  # ある文書中の単語の出現回数を格納するための配列
+fv_df = {}					  # 単語の出現文書数を格納するためのディクショナリ
+word_count = []				 # 単語の総出現回数を格納するための配列
+
+fv_tf_idf = []				  # ある文書中の単語の特徴量を格納するための配列
+
+count_flag = {}				 # fv_dfを計算する上で必要なフラグを格納するためのディクショナリ
+
 sql = "select ReviewId, AuthorId, Message "\
 	  "from Comment "\
 	  "where ReviewId < '100' "\
@@ -77,7 +68,8 @@ sql = "select ReviewId, AuthorId, Message "\
 csr.execute(sql)
 lines = csr.fetchall()
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='../treetagger')
-fv = {}
+
+txt_num = len(lines)
 
 for line in lines:
 	authorId = line[0]
@@ -105,12 +97,49 @@ for line in lines:
 	tags = treetaggerwrapper.make_tags(tagText, exclude_nottags=True)
 
 	#print words
+	fv = {}					 # Count of word
+	words = 0				   # total count of ward in line
+
+	for word in list(fv_df.keys()):
+		count_flag[word] = False
+
 	for tag in tags:
-		print(tag.lemma)
+		words += 1
+		word = tag.lemma
 
-        if fv.has_key(tag):
-            fv[tag]+=1
-        else:
-            fv[tag]=1
+		if fv.has_key(word):
+			fv[word]+=1
+		else:
+			fv[word]=1
 
-	print ""
+		if word in fv_df.keys(): # fv_dfにキー値がwordの要素があれば
+			if count_flag[word] == False: # フラグを確認し，Falseであれば
+				fv_df[word] += 1 # 出現文書数を1増やす
+				count_flag[word] = True # フラグをTrueにする
+		else:				 # fv_dfにキー値がwordの要素がなければ
+			fv_df[word] = 1 # 新たにキー値がwordの要素を作り，値として1を代入する
+			count_flag[word] = True # フラグをTrueにする
+
+	fv_tf.append(fv)
+	word_count.append(words)
+
+for txt_id, fv in enumerate(fv_tf):
+	tf = {}
+	idf = {}
+	tf_idf = {}
+	for key in list(fv.keys()):
+		tf[key] = float(fv[key]) / word_count[txt_id] # tfの計算
+		idf[key] = math.log(float(txt_num) / fv_df[key]) # idfの計算
+		tf_idf[key] = (tf[key] * idf[key], tf[key], idf[key], fv[key], fv_df[key]) # tf-idfその他の計算
+	tf_idf = sorted(list(tf_idf.items()), key=lambda x:x[1][0], reverse=True) # 得られたディクショナリtf-idfを、tf[key]*idf[key](tf-idf値)で降順ソート(処理後にはtf-idfはリストオブジェクトになっている)
+	fv_tf_idf.append(tf_idf)
+
+# 出力
+for txt_id, fv in enumerate(fv_tf_idf):
+	print ('This is the tf-idf of text', txt_id)
+	print ('total words:', word_count[txt_id])
+	print
+
+	for word, tf_idf in fv:
+		print ('%s\ttf-idf:%lf\ttf:%lf\tidf:%lf\tterm_count:%d\tdocument_count:%d' % (word, tf_idf[0], tf_idf[1], tf_idf[2], tf_idf[3], tf_idf[4])) # 左から順に、単語、tf-idf値、tf値、idf値、その文書中の単語の出現回数、その単語の出現文書数(これは単語ごとに同じ値をとる)
+	print
