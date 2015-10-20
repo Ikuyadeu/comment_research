02324f7afecc62aa4466b5598d4df3910292eff1
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-10-20 20:12:43 +0900

    make RQ2 data

--- R_sample/saner2015_RQ2_1.R
+++ R_sample/saner2015_RQ2_1.R
@@ -17,34 +17,34 @@ num <- read.csv("R_sample/numOfPatchsets_1_Start10000-End70705_RQ2.csv"
 # index_l_m: vote is "like" and status is "merge"
 # index_d_a: vote is "dislike" and status is abandoned
 index_l_m <- data.frame()
-index_l_a <- data.frame()
 index_d_m <- data.frame()
+index_l_a <- data.frame()
 index_d_a <- data.frame()
 
 for(i in 1:nrow(num)){
-  if(num$VotingScore[i] == 1){      # vote is like
-    if(num$Status[i] == "merged"){  # status is merge
+  if(num$Status[i] == "merged"){      # status is merge
+    if(num$VotingScore[i] == 1){      # vote is like
       index_l_m <- rbind(index_l_m, num[i,])
-    }else{                          # status is abandoned
-      index_l_a <- rbind(index_l_a, num[i,])
+    }else{                            # vote is dislike
+      index_d_m <- rbind(index_l_a, num[i,])
     }
-  }else{                            # vote is dislike
-    if(num$Status[i] == "merged"){
-      index_d_m <- rbind(index_d_m, num[i,])
+  }else{                              # status is abandoned
+    if(num$VotingScore[i] == 1){      # vote is like
+      index_l_a <- rbind(index_d_m, num[i,])
     }else{
       index_d_a <- rbind(index_d_a, num[i,])
     }
   }
 }
 
-# Output to PDF file
-boxplot(index_l_m$CurrentPar, xlab="Reviewer order", ylab="Reliability", main="Vote:like Status:merge")
-summary(index_l_m$CurrentPar)
-boxplot(index_l_a$CurrentPar, xlab="Reviewer order", ylab="Reliability", main="Vote:like: Status:abandoned")
-summary(index_l_a$CurrentPar)
-boxplot(index_d_m$CurrentPar, xlab="Reviewer order", ylab="Reliability", main="Vote:dislike Status:merge")
-summary(index_d_m$CurrentPar)
-boxplot(index_d_a$CurrentPar, xlab="Reviewer order", ylab="Reliability", main="Vote:dislike Status:abandoned")
-summary(index_d_a$CurrentPar)
+index_merge <- list(index_l_m, index_d_m)
+index_abondone <- list(index_l_a, index_d_a)
+index <- list(index_abondone, index_merge)
 
-dev.off()
+for(i in index){
+  for(j in i){
+    value = j$CurrentPar
+    boxplot(value, xlab="Reviewer order", ylab="Reliability", main=paste("Vote:", j$VotingScore[0], "Status:", j$Status[0]))
+    summary(value)
+  }
+}
---
+++ R_sample/saner2015_RQ2_2.R
@@ -0,0 +1,58 @@
+########################
+## SANER2015 RQ2
+## @author:Yuki Ueda
+## @createdOn:2015/10/20
+########################
+pdf("RQ2_2.pdf")
+
+
+### Set data
+num <- data.frame() # Init
+
+# Input numOfPatchsets_2_Start10000-End70705_RQ2.csv for csv
+num <- read.csv("R_sample/numOfPatchsets_2_Start10000-End70705_RQ2.csv"
+                 ,sep=",", header=TRUE)
+
+### number of patchset's voting(+1, -1) is 1
+# index_l_m: vote is "like" and status is "merge"
+# index_d_a: vote is "dislike" and status is abandoned
+index_l_m <- list(data.frame(), data.frame())
+index_d_m <- list(data.frame(), data.frame())
+index_l_a <- list(data.frame(), data.frame())
+index_d_a <- list(data.frame(), data.frame())
+
+for(i in 1:nrow(num)){
+  CIndex = num$CommentIndex[i]      # CommentIndex
+  if(num$Status[i] == "merged"){  # status is merge
+    if(num$VotingScore[i] == 1){      # vote is like
+      index_l_m[[CIndex]] <- rbind(index_l_m[[CIndex]], num[i,,])
+    }else{                          # vote is dislike
+      index_d_m[[CIndex]] <- rbind(index_d_m[[CIndex]], num[i,,])
+    }
+  }else{                            # status is abandoned
+    if(num$VotingScore[i] == 1){
+      index_l_a[[CIndex]] <- rbind(index_l_a[[CIndex]], num[i,,])
+    }else{
+      index_d_a[[CIndex]] <- rbind(index_d_a[[CIndex]], num[i,,])
+    }
+  }
+}
+
+# Summarize the index
+index_merge <- list(index_l_m, index_d_m)
+index_abondone <- list(index_l_a, index_d_a)
+index <- list(index_abondone, index_merge)
+
+# Output to PDF file
+for(i in index){
+  for(j in i){ # first Comment
+    value1 = j[[1]]$CurrentPar
+    for(k in i){ # second Comment
+      value2 = k[[2]]$CurrentPar
+      vote = paste("Vote:", j[[1]]$VotingScore[1], ",", k[[2]]$VotingScore[1], " ")
+      boxplot(value1, value2, xlab="Reviewer order", ylab="Reliability", main=paste(vote, "Status:", j[[1]]$Status[1]))
+      summary(value1)
+      summary(value2)
+    }
+  }
+}
