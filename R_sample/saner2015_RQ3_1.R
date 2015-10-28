0db33a37ecb241486cb3e20a2c7fbd88bff648c0
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-10-28 14:58:45 +0900

    fixed data

--- R_sample/saner2015_RQ3_1.R
+++ R_sample/saner2015_RQ3_1.R
@@ -10,7 +10,7 @@
 num <- data.frame() # Init
 
 # Input csv
-num <- read.csv("CSVdata_o/numOfPatchsets_1.csv"
+num <- read.csv("CSVdata_q/numOfPatchsets_1.csv"
                  ,sep=",", header=TRUE)
 
 ### number of patchset's voting(+1, -1) is 1
@@ -31,10 +31,43 @@ for(i in 1:nrow(num)){
 index <- list(index_like, index_dislike)
 
 for(i in index){
-  #value = i$CurrentPar
-  #name = paste("Vote:", i$VotingScore[1], "Status:", i$Status[1],"Num:",length(value),sep="")
-  #jpeg(paste("pictureRQ3_q/RQ3_1",name,".jpg",sep=""))
-  #boxplot(value, xlab="Reviewer order", ylab="Reliability", main=name, ylim=c(0,1))
-  cat(i$VotingScore[1])
-  print(summary(i$CurrentPar))
+  value = i$CurrentPar
+  name = paste("Vote:", i$VotingScore[1], "Status:", i$Status[1],"Num:",length(value),sep="")
+  jpeg(paste("pictureRQ3_q/RQ3_1",name,".jpg",sep=""))
+  boxplot(value, xlab="Reviewer order", ylab="Reliability", main=name, ylim=c(0,1))
+  print(name)
+  result <- summary(value)
+  print(result)
+
+  threshold = result[['1st Qu.']] - 1.5 * IQR(value)
+}
+
+
+### number of patchset's voting(+1, -1) is 1
+# index_like: vote is "like"
+# index_dislike_a: vote is "dislike"
+index_like <- data.frame()
+index_dislike <- data.frame()
+
+
+for(i in 1:nrow(num)){
+  if(num$CurrentPar[i] > threshold){
+    if(num$VotingScore[i] == 1){      # vote is like
+      index_like <- rbind(index_like, num[i,])
+    }else{                            # vote is dislike
+      index_dislike <- rbind(index_dislike, num[i,])
+    }
+  }
+}
+
+index <- list(index_like, index_dislike)
+
+for(i in index){
+  value = i$CurrentPar
+  name = paste("Vote:", i$VotingScore[1], "Status:", i$Status[1],"Num:",length(value),sep="")
+  jpeg(paste("pictureRQ3_q/RQ3_1",name,".jpg",sep=""))
+  boxplot(value, xlab="Reviewer order", ylab="Reliability", main=name, ylim=c(0,1))
+  print(name)
+  result <- min(value)
+  print(result)
 }
--- R_sample/saner2015_RQ3_2.R
+++ R_sample/saner2015_RQ3_2.R
@@ -43,8 +43,55 @@ for(i in 1:nrow(num)){
 # Output to PDF file
 for(i in index){ # first Comment
   for(j in i){  # second Comment
+    value = j$CurrentPar
     name = paste("Vote:", j$VotingScore[1], ",", j$VotingScore[2], "Num:",length(j$CurrentPar),sep = "")
     print(name)
-    print(summary(j$CurrentPar))
+    result <- summary(value)
+    print(result)
+
+    threshold = result[['1st Qu.']] - 1.5 * IQR(value)
+  }
+}
+
+
+
+
+### number of patchset's voting(+1, -1) is 1
+index_1 <- data.frame() # first vote is like or dislike
+index_2 <- list(index_1, index_1) # second vote is like or dislike
+index <- list(index_2,index_2)
+
+for(i in 1:nrow(num)){
+  Id = num$ReviewId[i]
+  CIndex = num$CommentIndex[i]      # CommentIndex
+  if(CIndex==1){
+    if(num$VotingScore[i] == 1){
+      vote1 = 1
+    }else{
+      vote1 = 2
+    }
+    next
+  }
+
+  if(num$VotingScore[i] == 1){
+    vote2 = 1
+  }else{
+    vote2 = 2
+  }
+
+  if(num$CurrentPar[i-1] > threshold && num$CurrentPar[i] > threshold){
+    index[[vote1]][[vote2]] <- rbind(index[[vote1]][[vote2]], num[i-1,])
+    index[[vote1]][[vote2]] <- rbind(index[[vote1]][[vote2]], num[i,])
+  }
+}
+
+# Output to PDF file
+for(i in index){ # first Comment
+  for(j in i){  # second Comment
+    value = j$CurrentPar
+    name = paste("Vote:", j$VotingScore[1], ",", j$VotingScore[2], "Num:",length(j$CurrentPar),sep = "")
+    print(name)
+    result <- min(value)
+    print(result)
   }
 }
