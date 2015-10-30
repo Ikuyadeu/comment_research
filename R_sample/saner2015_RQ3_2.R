c3892917a75d9857a2b5836d58726eb8c82d24b2
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-10-30 20:46:54 +0900

    fixed

--- R_sample/saner2015_RQ3_2.R
+++ R_sample/saner2015_RQ3_2.R
@@ -10,16 +10,13 @@
 num <- data.frame() # Init
 
 # Input csv
-num <- read.csv("CSVdata_o/numOfPatchsets_3.csv"
+num <- read.csv("CSVdata_o/numOfPatchsets_2.csv"
                  ,sep=",", header=TRUE)
 
 ### number of patchset's voting(+1, -1) is 1
-index_1 <- data.frame() # first vote is like or dislike
-index_2 <- list(index_1, index_1) # second vote is like or dislike
-index <- list(index_2,index_2)
+index <- list(list(data.frame(),data.frame()),list(data.frame(),data.frame()))
 
 for(i in 1:nrow(num)){
-  Id = num$ReviewId[i]
   CIndex = num$CommentIndex[i]      # CommentIndex
   if(CIndex==1){
     if(num$VotingScore[i] == 1){
@@ -50,47 +47,7 @@ for(i in index){ # first Comment
     print(result)
 
     threshold = result[['1st Qu.']] - 1.5 * IQR(value)
-  }
-}
-
-
-
-
-### number of patchset's voting(+1, -1) is 1
-index_1 <- data.frame() # first vote is like or dislike
-index_2 <- list(index_1, index_1) # second vote is like or dislike
-index <- list(index_2,index_2)
-
-for(i in 1:nrow(num)){
-  Id = num$ReviewId[i]
-  CIndex = num$CommentIndex[i]      # CommentIndex
-  if(CIndex==1){
-    if(num$VotingScore[i] == 1){
-      vote1 = 1
-    }else{
-      vote1 = 2
-    }
-    next
-  }
-
-  if(num$VotingScore[i] == 1){
-    vote2 = 1
-  }else{
-    vote2 = 2
-  }
-
-  if(num$CurrentPar[i-1] > threshold && num$CurrentPar[i] > threshold){
-    index[[vote1]][[vote2]] <- rbind(index[[vote1]][[vote2]], num[i-1,])
-    index[[vote1]][[vote2]] <- rbind(index[[vote1]][[vote2]], num[i,])
-  }
-}
-
-# Output to PDF file
-for(i in index){ # first Comment
-  for(j in i){  # second Comment
-    value = j$CurrentPar
-    name = paste("Vote:", j$VotingScore[1], ",", j$VotingScore[2], "Num:",length(j$CurrentPar),sep = "")
-    print(name)
+    value = subset(j, CurrentPar > threshold)$CurrentPar
     result <- min(value)
     print(result)
   }
