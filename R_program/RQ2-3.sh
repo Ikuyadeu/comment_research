45c77439fc726cae80367cdeaf1edfbe653f6f28
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-11-07 13:02:16 +0900

    check

--- R_program/RQ2-3.sh
+++ R_program/RQ2-3.sh
@@ -3,5 +3,5 @@ R --vanilla --slave < R_program/saner2015_RQ2_1.R
 R --vanilla --slave < R_program/saner2015_RQ2_2.R
 R --vanilla --slave < R_program/saner2015_RQ2_3.R
 
-R --vanilla --slave < R_program/saner2015_RQ3_1.R
-R --vanilla --slave < R_program/saner2015_RQ3_2.R
+#R --vanilla --slave < R_program/saner2015_RQ3_1.R
+#R --vanilla --slave < R_program/saner2015_RQ3_2.R
