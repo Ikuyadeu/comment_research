5da7a42b8fb56388f2c01b8681bd105570d7025d
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2015-12-24 19:03:42 +0900

    output firstdata

--- GetReviewersReliability.sh
+++ GetReviewersReliability.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python GetReviewersReliability.py qt 1 > CSVdata_q_3/numOfPatchsets_1.csv
-python GetReviewersReliability.py qt 2 > CSVdata_q_3/numOfPatchsets_2.csv
-python GetReviewersReliability.py qt 3 > CSVdata_q_3/numOfPatchsets_3.csv
+python GetReviewersReliability.py qt 1 > CSVdata_oss/numOfPatchsets_qt_1.csv
+python GetReviewersReliability.py qt 2 > CSVdata_oss/numOfPatchsets_qt_2.csv
+python GetReviewersReliability.py qt 3 > CSVdata_oss/numOfPatchsets_qt_3.csv
 
-python GetReviewersReliability.py Openstack 1 > CSVdata_o_3/numOfPatchsets_1.csv
-python GetReviewersReliability.py Openstack 2 > CSVdata_o_3/numOfPatchsets_2.csv
-python GetReviewersReliability.py Openstack 3 > CSVdata_o_3/numOfPatchsets_3.csv
+python GetReviewersReliability.py Openstack 1 > CSVdata_oss/numOfPatchsets_os_1.csv
+python GetReviewersReliability.py Openstack 2 > CSVdata_oss/numOfPatchsets_os_2.csv
+python GetReviewersReliability.py Openstack 3 > CSVdata_oss/numOfPatchsets_os_3.csv
