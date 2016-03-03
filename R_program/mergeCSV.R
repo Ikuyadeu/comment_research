0c16f4de1e2ba4b05c8fd8ae093f98aaa5164bb1
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-03-03 20:53:51 +0900

    fixed

--- R_program/mergeCSV.R
+++ R_program/mergeCSV.R
@@ -2,8 +2,8 @@ data1 <- data.frame()
 data2 <- data.frame()
 # data1 = read.csv("CSVdata_journal/example2.csv", sep=",", header=TRUE)
 # data2 = read.csv("CSVdata_journal/example1.csv", sep=",", header=TRUE)
-data1 = read.csv("CSVdata_journal/expertiseLevel_qt.csv", sep=",", header=TRUE)
-data2 = read.csv("CSVdata_journal/Qt.csv", sep=",", header=TRUE)
+data1 = read.csv("CSVdata_journal/Qt.csv", sep=",", header=TRUE)
+data2 = read.csv("CSVdata_journal/expertiseLevel_qt.csv", sep=",", header=TRUE)
 
 # merge Two data
 output <- merge(x = data1, y = data2, by = c("ReviewId", "ReviewerId"), all.x = TRUE)
