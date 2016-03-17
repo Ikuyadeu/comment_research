e6581e9ff5ef78c38697d01c9cdd0ddc4d649803
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-03-17 13:47:37 +0900

    reserch expertlevel

---
+++ R_program/relation_exp_correct.R
@@ -0,0 +1,26 @@
+reviewers1 <- data.frame()
+reviewers1 <- read.csv("CSVdata_journal/merged_qt2.csv", sep=",", header=TRUE)
+
+# get idList more than 5 %(725 * 0.05)
+idlist <- data.frame()
+idlist <- read.csv("CSVdata_journal/reviewerList_BasedOn_freqOfReview.csv", sep=",", header=TRUE)
+idlist <- head(idlist, n=nrow(idlist) * 0.05)
+
+
+
+reviewers <- data.frame()
+# reviewers <- reviewers1
+for(i in idlist$id){
+  reviewers <- rbind(reviewers, subset(reviewers1, reviewers1$ReviewerId == i))
+}
+
+# each ExpertiseLevel
+current <- data.frame()
+incurrent <- data.frame()
+
+current <- subset(reviewers, reviewers$IncurrentVote == 0, ExpertiseLevel)
+incurrent <- subset(reviewers, reviewers$IncurrentVote == 1, ExpertiseLevel)
+print(nrow(current))
+print(nrow(incurrent))
+pdf("jounal.pdf")
+boxplot(current$ExpertiseLevel, incurrent$ExpertiseLevel)
